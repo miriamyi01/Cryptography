1318f7e4d0383afe90c4804f5f12b2438b0f68a4
miriamyi01 <54131192+miriamyi01@users.noreply.github.com>
2024-05-17 23:28:09 -0600

    04 - Hashing Files
    Using the OpenSSL Command Line Utilities do the following tasks:
    
    1. Using the Asymmetric Keys from the previous practical session, sign and verify a text file.
    2. Encrypt a text file using AES-256.
    3. Decrypt a text file using AES-256.
    4. Get the hash of a text file using SHA2 of 512 bits.
    5. Verify the previous hash.

--- 04 - Hashing Files/Practica_04.sh
+++ 04 - Hashing Files/Practica_04.sh
@@ -1,7 +1,14 @@
-openssl dgst -sha256 -sign private_key.pem -out firma.sha256 Texto.txt
-openssl dgst -sha256 -verify public_key.pem -signature firma.sha256 Texto.txt
-openssl enc -aes-256-cbc -salt -in Texto.txt -out cifrado.txt -k miriam1234
-openssl enc -d -aes-256-cbc -in cifrado.txt -out descifrado.txt -k miriam1234
-openssl dgst -sha512 -hex Texto.txt
-openssl dgst -sha512 -sign private_key.pem -out hash.sha512 Texto.txt
-openssl dgst -sha512 -hex -verify public_key.pem -signature hash.sha512 Texto.txt
+type private_key.pem
+type public_key.pem
+type texto.txt
+openssl dgst -sha256 -sign private_key.pem -out firma.sha256 texto.txt
+type firma.sha256
+openssl dgst -sha256 -verify public_key.pem -signature firma.sha256 texto.txt
+openssl enc -aes-256-cbc -salt -in texto.txt -out cifrado.txt -k miriam1234 -pbkdf2
+type cifrado.txt
+openssl enc -d -aes-256-cbc -in cifrado.txt -out descifrado.txt -k miriam1234 -pbkdf2
+type descifrado.txt
+openssl dgst -sha512 -hex texto.txt
+openssl dgst -sha512 -sign private_key.pem -out hash.sha512 texto.txt
+type hash.sha512
+openssl dgst -sha512 -hex -verify public_key.pem -signature hash.sha512 texto.txt
